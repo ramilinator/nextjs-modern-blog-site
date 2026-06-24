8569f78f4a71560b23e7836b13d6085aa0e8a3d0
Ramil Aoanan <ramilaoanan@gmail.com>
2026-06-24 18:52:26 +0800

    Configure Cloudinary uploads

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "strapi-cms-w2tu.onrender.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
